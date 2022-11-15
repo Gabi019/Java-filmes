e086732dc695d3706339671f28f44888b69767c7
Gabriela <gabriellajhonatan015@gmail,com>
2022-11-14 22:55:28 -0300

    importacao inicial

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main (String[] args) throws Exception{
+
+        // fazer uma conexão HTTP
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // extrair so os dados que interessam ( titulo, poster,
+        // classificação) parcial, extrair os dados.
+        JsonParser jsonParcer = new jsonParcer();
+        List<Map<String, String>> listaDeFilmes = body;
+
+        // exibir e manipular os dados.
+    }
+}
---
+++ src/jsonParcer.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.Map;
+
+public class jsonParcer {
+    public List<Map<String, String>> parse(){
+
+    }
+}
